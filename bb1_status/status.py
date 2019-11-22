7e5c6e2e885308827cf1663ba54a80682556538f
George Knowlden <george.knowlden@gmail.com>
2019-11-22 10:58:25 +0800

    Fixed the publisher/subscriber, and made a launch file

--- bb1_status/status.py
+++ bb1_status/status.py
@@ -4,14 +4,18 @@ import message_filters
 from vesc_msgs.msg import VescStateStamped
 from bb1_status.msg import status
 
-empty =  {
+temperature = {
 	"FL": 0,
 	"BL": 0,
 	"FR": 0,
 	"BR": 0
 }
-temperature = empty
-voltage = empty
+voltage = {
+	"FL": 0,
+	"BL": 0,
+	"FR": 0,
+	"BR": 0
+}
 
 def average(dictionary):
 	total = 0
@@ -31,40 +35,32 @@ def maximum(dictionary):
 def callback(data, wheel):
 	temperature[wheel] = data.state.temperature_pcb
 	voltage[wheel] = data.state.voltage_input
-	rospy.loginfo("Avg temperature: %s", round(average(temperature), 1))
-	rospy.loginfo("Max temperature: %s", round(maximum(temperature), 1))
-	rospy.loginfo("Avg voltage: %s", round(average(voltage), 1))
 
-def listener():
+def listener_talker():
 
-	rospy.init_node("listener", anonymous = True)
+	rospy.init_node("status", anonymous = True)
 
 	rospy.Subscriber("front_left_wheel/sensors/core", VescStateStamped, callback, ("FL"))
 	rospy.Subscriber("back_left_wheel/sensors/core", VescStateStamped, callback, ("BL"))
 	rospy.Subscriber("front_right_wheel/sensors/core", VescStateStamped, callback, ("FR"))
 	rospy.Subscriber("back_right_wheel/sensors/core", VescStateStamped, callback, ("BR"))
 
-	rospy.spin()
+	r = rospy.Rate(1) #1Hz
 
-def talker():
-	# this needs to be made into a seperate thing, and then they both get launched together
-
-	pub = rospy.Publisher("bb1_status", status)
-	rospy.init_node("talker", anonymous = True)
-	r = rospy.Rate(2) #2Hz
-	msg = status()
-	msg.average_voltage = round(average(voltage), 1)
-	msg.maximum_temperature = round(maximum(temperature), 1)
-	msg.average_temperature = round(average(temperature), 1)
+	pub = rospy.Publisher("bb1_status", status, queue_size = 10)
 
 	while not rospy.is_shutdown():
-		rospy.loginfo(msg)
+
+		msg = status()
+		msg.average_voltage = average(voltage)
+		msg.maximum_temperature = maximum(temperature)
+		msg.average_temperature = average(temperature)
+
 		pub.publish(msg)
+
 		r.sleep()
 
-if __name__ == "__main__":
-	listener()
+	rospy.spin()
 
-	try:
-		talker()
-	except rospy.ROSInterruptException: pass
+if __name__ == "__main__":
+	listener_talker()
