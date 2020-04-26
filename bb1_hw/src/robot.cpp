29e01748310d5621a3d3d31e9a2ceffc6397a868
George Knowlden <george.knowlden@gmail.com>
2020-04-26 11:23:06 +0800

    Better response to cmd_vel

--- bb1_hw/src/robot.cpp
+++ bb1_hw/src/robot.cpp
@@ -106,10 +106,10 @@ int main(int argc, char** argv)
   // Setup separate queue and single-threaded spinner to process timer callbacks
   // that interface with BB1 hardware
   ros::CallbackQueue bb1_queue;
-  ros::AsyncSpinner spinner(2);//, &bb1_queue);
+  ros::AsyncSpinner spinner(4);//, &bb1_queue);
 
   ros::Time last_time = ros::Time::now();
-  ros::Timer control_loop = nh.createTimer(ros::Duration(0.005), 
+  ros::Timer control_loop = nh.createTimer(ros::Duration(0.01), 
     boost::bind(controlLoop, last_time, boost::ref(cm), boost::ref(robot)));
 
   ROS_INFO_STREAM_NAMED("hardware_interface","Starting spinner");
